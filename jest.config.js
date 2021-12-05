6c432dad3383a43d639241c51b013c4790b6286e
Vijay Menon <vijaymenonx@gmail.com>
2021-12-05 12:24:24 -0800

    Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx|js)$": "ts-jest",
+  },
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
 };
